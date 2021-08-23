7b10561a1e368d929a40e8a3a493a2cbd7a844ac
islam-abdelkader <islamabdelkader22@gmail.com>
2021-08-23 17:44:20 +0200

    add Companyclass and test binding

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CompanyInfo } from 'src/app/Shared/company-info';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  myComp: CompanyInfo;
+  isSevicesDisplay: boolean;
+  btnText: string;
+  constructor() {
+    this.isSevicesDisplay = false;
+    this.btnText = 'Show Services';
+    this.myComp = new CompanyInfo('ITI',
+      'https://placekitten.com/250/250',
+      ['marketing', 'hosting', 'learning'])
+  }
 
   ngOnInit(): void {
   }
-
+  ToggleServices() {
+    this.isSevicesDisplay = !this.isSevicesDisplay;
+    if (this.isSevicesDisplay) {
+      this.btnText = 'Hide Services'
+    }
+    else {
+      this.btnText = 'Show Services'
+    }
+  }
 }
---
+++ src/app/Shared/company-info.ts
@@ -0,0 +1,5 @@
+export class CompanyInfo {
+  constructor(public name: string, public image: string, public services: string[]) {
+
+  }
+}
