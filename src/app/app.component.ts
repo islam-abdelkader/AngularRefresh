4d7301051d7c51499f03a8e26e0b81bb2b599b52
islam-abdelkader <islamabdelkader22@gmail.com>
2021-08-23 16:18:57 +0200

    Starting and adding Layout component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  // template: '<h3> Hi again</h3>',
+  // styles: ['h3{color:red}']
 })
 export class AppComponent {
   title = 'AngularProject';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Components/Layout/header/header.component';
+import { FooterComponent } from './Components/Layout/footer/footer.component';
+import { ContentComponent } from './Components/Layout/content/content.component';
+import { HomeComponent } from './Components/home/home.component';
+import { SidebarComponent } from './Components/Layout/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContentComponent,
+    HomeComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
